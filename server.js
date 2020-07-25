684011ad4bf42b1f5540ac4210a1f486cec04047
Nicolai <nmahinay@gmail.com>
2020-07-25 14:46:59 +0800

    sentiment and addition completed

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require ('express')
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+
+let app=express();
+let app2=express();
+
+app.use(express.static('public'));
+
+let addition=function(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+let sentimentAnalysis=function(text){
+    let result=sentiment.analyze(text);
+    return result;
+}
+
+app.get('/analyze',function(request,response){
+    let text=request.query.text;
+    let resultAnalysis=sentimentAnalysis(text);
+    response.json(resultAnalysis);
+})
+
+app.get('/adder', function(request,response){
+    let num1 = parseInt(request.query.num1);
+    let num2 = parseInt(request.query.num2);
+    console.log(num1)
+    console.log(num2)
+    let myResult=addition(num1,num2);
+    response.json({result:myResult})
+})
+
+app.get('/test', function(request,response){
+    response.send('This is the test endpoint')
+
+})
+
+//start the server on port 3000
+app.listen(3000);
+
